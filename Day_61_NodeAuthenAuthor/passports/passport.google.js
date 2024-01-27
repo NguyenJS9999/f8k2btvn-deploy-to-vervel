2b4eaed280df6418094f70faed9d0aa5c3bf0806
Nguyễn Trung Nguyên <31590813+NguyenKeo@users.noreply.github.com>
2024-01-27 15:37:38 +0700

    first commit Day61 Authen-Author

---
+++ Day_61_NodeAuthenAuthor/passports/passport.google.js
@@ -0,0 +1,23 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy
+const { User } = require("../models/index")
+
+module.exports = new GoogleStrategy(
+  {
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    // callbackURL: "https://day61.vercel.app/auth/google/callback",
+
+  },
+  async function (accessToken, refreshToken, profile, cb) {
+    const { name, email } = profile._json
+    const [user, created] = await User.findOrCreate({
+      where: { provider: "google", email },
+      defaults: {
+        fullname: name,
+        email,
+        provider: "google",
+      },
+    })
+    return cb(null, user)
+  }
+)
